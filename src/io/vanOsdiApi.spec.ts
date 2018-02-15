7b968cfd2df50eaf83d33bf44a0fee529d12671a
Isaac Pohl-Zaretsky <isaacpz@stanford.edu>
2018-02-15 13:07:31 -0800

    Update vanOsdiApi.spec.ts

--- src/io/vanOsdiApi.spec.ts
+++ src/io/vanOsdiApi.spec.ts
@@ -15,7 +15,7 @@ describe("Generic OSDI API", async () => {
   let client: OsdiApiClient;
   it("should be created", () => {
     expect(function() {
-      client = new VanOsdiApi("84606d9f-e019-59a1-f817-88f8b69806d4");
+      client = new VanOsdiApi("YOUR_API_KEY");
     }).to.not.throw();
   });
 
